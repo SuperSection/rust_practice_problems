fccb5e5eb4c893b8aa874a27f2de1573123dc7ae
Soumo Sarkar <soumosarkar297@gmail.com>
2025-08-19 20:47:49 +0530

    01(Ans): refactor input type from u32 to i32

--- 01_is_even.rs
+++ 01_is_even.rs
@@ -6,15 +6,15 @@
 
 
 fn main() {
-    let num = 2100234567;
-    let ans = is_even(num);
+    let num: i32 = 510;
+    let ans: bool = is_even(num);
     println!("{} {} an even number", num, if ans { "is" } else { "is NOT" });
 }
 
 
 // rust follows snake_case_notation
 
-fn is_even(num: u32) -> bool {
+fn is_even(num: i32) -> bool {
     if num % 2 == 0 {
         return true;
     } else {
